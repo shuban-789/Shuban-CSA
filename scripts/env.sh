3b0a09f6ded59205953cf27a3db58910c7d455e8
shuban-789 <cybergeek11929@gmail.com>
2024-08-26 12:43:28 -0700

    env tool check commit and repository organization

---
+++ scripts/env.sh
@@ -0,0 +1,9 @@
+%%script bash
+
+# Dependency Variables, set to match your project directories
+
+cat <<EOF > /tmp/variables.sh
+export project_dir=$HOME/CSA
+export project=\$project_dir/Shuban-CSA
+export project_repo="https://github.com/nighthawkcoders/portfolio_2025.git"
+EOF
