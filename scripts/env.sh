e05b09455c427ed46db7173d85647a561ee4102e
shuban-789 <cybergeek11929@gmail.com>
2024-08-27 02:01:41 -0700

    edit to env.sh verification script

--- scripts/env.sh
+++ scripts/env.sh
@@ -2,8 +2,17 @@
 
 # Dependency Variables, set to match your project directories
 
+# Load environment variables
 cat <<EOF > /tmp/variables.sh
 export project_dir=$HOME/CSA
 export project=\$project_dir/Shuban-CSA
 export project_repo="https://github.com/shuban-789/Shuban-CSA.git"
 EOF
+
+# Verify environment variables after sourcing
+source /tmp/variables.sh
+
+# Output shown title and value variables
+echo "Project dir: $project_dir"
+echo "Project: $project"
+echo "Repo: $project_repo"
